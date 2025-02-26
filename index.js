28520cbe9f2e71122783e6688499f6703df7329f
Ehtesham86 <buttehtesham86@gmail.com>
2025-02-25 18:06:14 -0800

    3 18

--- index.js
+++ index.js
@@ -20,7 +20,12 @@ app.use(cors());
 mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true })
   .then(() => console.log('Connected to MongoDB'))
   .catch((err) => console.error('MongoDB connection error:', err));
-
+  app.use(function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "X-Requested-With");
+    next();
+    });
+  
 app.use('/api/auth', authRoutes);
 app.use('/api', adminRoutes);
 
