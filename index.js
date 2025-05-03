6cdfac39501ec0f59769e9813bc17e77cdfbccbd
Ehtesham86 <buttehtesham86@gmail.com>
2025-05-03 07:54:30 -0700

    assa

--- index.js
+++ index.js
@@ -15,7 +15,7 @@ app.use('/uploads', express.static(path.join(__dirname, 'uploads')));
 
 
 app.use(express.json());
-app.use(cors());
+app.options('*', cors()); // Make sure OPTIONS preflight requests are handled
 
 mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true })
   .then(() => console.log('Connected to MongoDB'))
